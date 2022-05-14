84b91bc3317995a18cbfa2dad52cd23f1a39130e
martins0n <33594071+martins0n@users.noreply.github.com>
2022-05-14 18:01:57 +0300

    ADD: ports and address

--- server/main.go
+++ server/main.go
@@ -6,20 +6,25 @@ import (
 	"github.com/martins0n/openvpn-traffic-viewer/lib"
 )
 
-
-func  ShowStats(logPath string) *[]lib.OpenVpnStatus {
+func ShowStats(logPath string) *[]lib.OpenVpnStatus {
 	openVpnStatusList := lib.ParseOpenVpnStatus(logPath)
 	return openVpnStatusList
 }
 
 func main() {
 	var logPath string
+	var port string
+	var address string
+
 	flag.StringVar(&logPath, "log", "", "log file path")
+	flag.StringVar(&port, "port", "8080", "port")
+	flag.StringVar(&address, "address", "0.0.0.0", "address")
+
 	flag.Parse()
 
 	r := gin.Default()
 	r.GET("/showstats", func(c *gin.Context) {
 		c.JSON(200, ShowStats(logPath))
 	})
-	r.Run()
+	r.Run(address + ":" + port)
 }
